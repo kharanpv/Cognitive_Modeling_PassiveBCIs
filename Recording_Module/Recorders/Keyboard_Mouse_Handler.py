b4b287e70ae24c3306e0f3193f5163b1415878db
Prateek <kharanpv@mail.uc.edu>
2025-05-22 20:16:21 -0400

    Logging of keystrokes complete

--- Recording_Module/Recorders/Keyboard_Mouse_Handler.py
+++ Recording_Module/Recorders/Keyboard_Mouse_Handler.py
@@ -1,11 +1,30 @@
-class Keyboard_Mouse_Handler:
-    class Keyboard_Handler:
-        def __init__():
-            pass
+from pynput import keyboard
+import pandas as pd
+import numpy as np
+import time
 
-    class Mouse_Handler:
-        def __init__():
-            pass
+class Keyboard_Handler:
+    def __init__(self):
+        self.key_log = pd.DataFrame(column=['time', 'key', 'event'])
+        self.listner = keyboard.Listener()
+    
+    def on_press(self, key): 
+        if isinstance(key, keyboard.KeyCode):     
+            new_entry =   pd.DataFrame({'time': [pd.Timestamp.now()], 'key': [key.char], 'event': ['press']})
+        elif isinstance(key, keyboard.Key):
+            new_entry =   pd.DataFrame({'time': [pd.Timestamp.now()], 'key': [key], 'event': ['press']})
 
+        self.key_log = pd.concat([self.key_log, new_entry], ignore_index=True)
+
+    def on_release(self, key): 
+        if isinstance(key, keyboard.KeyCode):     
+            new_entry =   pd.DataFrame({'time': [pd.Timestamp.now()], 'key': [key.char], 'event': ['release']})
+        elif isinstance(key, keyboard.Key):
+            new_entry =   pd.DataFrame({'time': [pd.Timestamp.now()], 'key': [key], 'event': ['release']})
+
+        self.key_log = pd.concat([self.key_log, new_entry], ignore_index=True)
+
+class Mouse_Handler:
     def __init__():
         pass
+

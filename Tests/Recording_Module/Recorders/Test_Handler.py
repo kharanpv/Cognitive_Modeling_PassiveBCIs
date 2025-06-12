3ae7614678762170483ad46a87524bfe548a11c4
Prateek <kharanpv@mail.uc.edu>
2025-06-12 19:38:34 -0400

    Base Handler class testing complete

--- Tests/Recording_Module/Recorders/Test_Handler.py
+++ Tests/Recording_Module/Recorders/Test_Handler.py
@@ -1,6 +1,7 @@
 import pytest
-from multiprocessing import Event, connection
+from multiprocessing import connection
 import multiprocessing
+from unittest.mock import patch, MagicMock
 
 from Recording_Module.Recorders.Handler import Handler
 
@@ -8,9 +9,11 @@ class DummyHandler(Handler):
     def _run_listener(self, stop_event, pipe_conn):
         return 'Dummy Method'
 
+
 @pytest.fixture
 def handler():
     return DummyHandler()
+
 
 def test_init(handler):
 
@@ -19,3 +22,39 @@ def test_init(handler):
     assert isinstance(handler.active, bool)
     assert isinstance(handler.parent_conn, connection.Connection)
     assert isinstance(handler.child_conn, connection.Connection)
+
+
+@patch('Recording_Module.Recorders.Handler.Process')
+def test_start_trigger(mock_process, handler):
+
+    handler.stop_event = MagicMock()
+    handler.child_conn = MagicMock()
+    handler.trigger_listener('start')
+
+    mock_process.assert_called_once_with(
+        target=handler._run_listener,
+        args=(handler.stop_event, handler.child_conn),
+        daemon=True
+    )
+
+    mock_process.return_value.start.assert_called_once()
+
+
+@patch('Recording_Module.Recorders.Handler.Process')
+@patch('Recording_Module.Recorders.Handler.Event')
+def test_stop_trigger(mock_event, mock_process, handler):
+
+    mock_event = MagicMock()
+    mock_process = MagicMock()
+
+    handler.stop_event = mock_event
+    handler.process = mock_process
+    handler.parent_conn = MagicMock()
+    handler.active = True
+
+    handler.trigger_listener('stop', '/dummy_location')
+
+    mock_event.set.assert_called_once()
+    handler.parent_conn.send.assert_called_once_with('/dummy_location')
+    mock_process.join.assert_called_once()
+    assert handler.active is False
